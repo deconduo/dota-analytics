fcec9d959fd457fa856ec69c193970b7dc74e0d7
deconduo <deconduo@tcd.ie>
2013-10-10 23:55:06 +0000

    Minor bug fixes

--- dota-analytics/main.py
+++ dota-analytics/main.py
@@ -3,14 +3,15 @@
 import urllib
 import json
 import csv
+from time import sleep
 
 '''Variables'''
 
-APIkey = '' # Enter your Steam API key here
-numberOfMatches = # Enter number of matches to download
-startMatch = "" # Enter the  ID of the match to start at
-gameMode = "" # Enter the number corresponding to the appropriate gamemode
-matchBracket = "" # Enter the number corresponding the match bracket
+APIkey = "" # Enter your Steam API key here
+numberOfMatches = 100 # Enter number of matches to download
+dateStart = 1381363200 # Enter the date to start at, in UNIX time
+gameMode = 1 # Enter the number corresponding to the appropriate gamemode
+matchBracket = 0 # Enter the number corresponding the match bracket
 
 # Steam API URLs
 matchHistoryURL = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?key="
@@ -22,15 +23,17 @@ matchDetailsURL = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/
 
 def getMatchList():
 	matchList = []
-
 	i = 0
+	dateMax = dateStart
 	while i < numberOfMatches:
-		matchListRequest = matchHistoryURL + APIkey + "&start_at_match_id=" + str(startMatch + i) + "&game_mode=" + gameMode + "&skill=" + matchBracket + "&lobby_type=0&matches_requested=25"
+		matchListRequest = matchHistoryURL + APIkey + "&date_max=" + str(dateMax) + "&game_mode=" + str(gameMode) + "&skill=" + str(matchBracket) + "&lobby_type=0&matches_requested=25"
 		print matchListRequest
 		matchListFile = urllib.urlopen(matchListRequest)
 		matchListText = matchListFile.read()
+		print matchListText
 		jsonListText = json.loads(matchListText)
 		j = 0
+		dateMax = jsonListText['result']['matches'][24]['start_time']
                 while j < 25:
 			jsonMatch = jsonListText['result']['matches'][j]['match_id']
 			matchList.append(jsonMatch)
@@ -57,6 +60,7 @@ def getMatchData(matchList):
 
 		j = 0
 		while j < 10:
+			sleep(1)
 			heroDetails = []
 			heroDetails.append(jsonDetailsText['result']['match_id'])
 			heroDetails.append(jsonDetailsText['result']['players'][j]['hero_id'])
@@ -70,14 +74,14 @@ def getMatchData(matchList):
 			j += 1
 
 			heroOutput = open('heroData.csv', "wb")
-    		heroWriter = csv.writer(heroOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
-    		matchWriter.writerow(heroDetails)
-    		heroOutput.close()
+    			heroWriter = csv.writer(heroOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+    			heroWriter.writerow(heroDetails)
+    			heroOutput.close()
 
-	matchOutput = open('matchData.csv', "wb")
-    matchWriter = csv.writer(matchOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
-	heroWriter.writerow(matchDetails)
-    matchOutput.close()
+		matchOutput = open('matchData.csv', "wb")
+	    	matchWriter = csv.writer(matchOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+		matchWriter.writerow(matchDetails)
+    		matchOutput.close()
 
 # Main program
 
