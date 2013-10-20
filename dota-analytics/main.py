693df31a304fcb5afcef2c49bbfc1193840540ee
deconduo <deconduo@tcd.ie>
2013-10-20 18:24:20 +0000

    updated main.py, fixed csv problems

--- dota-analytics/main.py
+++ dota-analytics/main.py
@@ -22,74 +22,81 @@ matchDetailsURL = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/
 # Retrieves a list of matches to process, given the starting variables.
 
 def getMatchList():
-	matchList = []
-	i = 0
-	dateMax = dateStart
-	while i < numberOfMatches:
-		matchListRequest = matchHistoryURL + APIkey + "&date_max=" + str(dateMax) + "&game_mode=" + str(gameMode) + "&skill=" + str(matchBracket) + "&lobby_type=0&matches_requested=25"
-		print matchListRequest
-		matchListFile = urllib.urlopen(matchListRequest)
-		matchListText = matchListFile.read()
-		print matchListText
-		jsonListText = json.loads(matchListText)
-		j = 0
-		dateMax = jsonListText['result']['matches'][24]['start_time']
+        matchList = []
+        i = 0
+        dateMax = dateStart
+        while i < numberOfMatches:
+                matchListRequest = matchHistoryURL + APIkey + "&date_max=" + str(dateMax) + "&game_mode=" + str(gameMode) + "&skill=" + str(matchBracket) + "&lobby_type=0&matches_requested=25"
+                print matchListRequest
+                matchListFile = urllib.urlopen(matchListRequest)
+                matchListText = matchListFile.read()
+                print matchListText
+                jsonListText = json.loads(matchListText)
+                j = 0
+                dateMax = jsonListText['result']['matches'][24]['start_time']
                 while j < 25:
-			jsonMatch = jsonListText['result']['matches'][j]['match_id']
-			matchList.append(jsonMatch)
+                        jsonMatch = jsonListText['result']['matches'][j]['match_id']
+                        matchList.append(jsonMatch)
                         j+=1
-		i += 25
+                i += 25
 
-	return matchList
+        return matchList
 
-# Gets match information and writes it to a csv file.
+# Gets match and hero information
 
 def getMatchData(matchList):
-	for item in matchList:
-		matchDetails = []
-		currentMatch = str(item)
-		matchDetailsRequest = matchDetailsURL + APIkey + "&match_id=" + currentMatch
-		matchDetailsFile = urllib.urlopen(matchDetailsRequest)
-		matchDetailsText = matchDetailsFile.read()
+        for item in matchList:
+                matchInfo = []
+                heroInfo = []
+                currentMatch = str(item)
+                matchDetailsRequest = matchDetailsURL + APIkey + "&match_id=" + currentMatch
+                matchDetailsFile = urllib.urlopen(matchDetailsRequest)
+                matchDetailsText = matchDetailsFile.read()
+                try: jsonDetailsText = json.loads(matchDetailsText)
+                    matchDetails = []
+                    matchDetails.append(jsonDetailsText['result']['match_id'])
+                    matchDetails.append(jsonDetailsText['result']['radiant_win'])
+                    matchDetails.append(jsonDetailsText['result']['duration'])
+                    matchDetails.append(jsonDetailsText['result']['first_blood_time'])
+                    matchinfo.append(matchDetails)
 
-		jsonDetailsText = json.loads(matchDetailsText)
-		matchDetails.append(jsonDetailsText['result']['match_id'])
-		matchDetails.append(jsonDetailsText['result']['radiant_win'])
-		matchDetails.append(jsonDetailsText['result']['duration'])
-		matchDetails.append(jsonDetailsText['result']['first_blood_time'])
 
-		j = 0
-		while j < 10:
-			sleep(1)
-			heroDetails = []
-			heroDetails.append(jsonDetailsText['result']['match_id'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['hero_id'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['kills'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['deaths'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['assists'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['gold_per_min'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['xp_per_min'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['last_hits'])
-			heroDetails.append(jsonDetailsText['result']['players'][j]['denies'])
-			j += 1
+                    j = 0
+                    while j < 10:
+                            heroDetails = []
+                            heroDetails.append(jsonDetailsText['result']['match_id'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['hero_id'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['kills'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['deaths'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['assists'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['gold_per_min'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['xp_per_min'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['last_hits'])
+                            heroDetails.append(jsonDetailsText['result']['players'][j]['denies'])
+                            heroInfo.append(heroDetails)
+                            j += 1
 
-			heroOutput = open('heroData.csv', "wb")
-    			heroWriter = csv.writer(heroOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
-    			heroWriter.writerow(heroDetails)
-    			heroOutput.close()
+                except:
+                    print matchDetailsText
+                    sleep(5)
+        writeCSV(matchInfo, "match")
+        writeCSV(heroInfo, "hero")
 
-		matchOutput = open('matchData.csv', "wb")
-	    	matchWriter = csv.writer(matchOutput, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
-		matchWriter.writerow(matchDetails)
-    		matchOutput.close()
+
+
+def writeCSV(details, filename):
+    Output = open(filename+'.csv', "wb")
+    Writer = csv.writer(Output, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
+    Writer.writerow(details)
+    Output.close()
 
 # Main program
 
 def main():
-	matchList = getMatchList()
-	print matchList
+        matchList = getMatchList()
+        print matchList
 
-	getMatchData(matchList)
+        getMatchData(matchList)
 
 
 if __name__ == "__main__":
